719147f47d187b918cb739ff1f84070221991003
Timmy Nguyen <timmypass17@gmail.com>
2024-07-14 20:13:47 -0700

    Added google sign in

--- Topaz/ViewControllers/WishlistViewController.swift
+++ Topaz/ViewControllers/WishlistViewController.swift
@@ -6,100 +6,52 @@
 //
 
 import UIKit
-import CommonCrypto
-import SafariServices
+import FirebaseCore
+import GoogleSignIn
+import FirebaseAuth
 
 class WishlistViewController: UIViewController {
     
+    let button: GIDSignInButton = {
+        let button = GIDSignInButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let codeVerifier = generateCodeVerifier()
-        let codeChallenge = createCodeChallenge(from: codeVerifier)
-//        storeCodeVerifierInCookie(codeVerifier)
-
-        print("Code Verifier: \(codeVerifier)")
-        print("Code Challenge: \(codeChallenge)")
+        button.addAction(didTapGoogleSignIn(), for: .touchUpInside)
+        view.addSubview(button)
         
-        if let authorizationURL = buildAuthorizationURL(codeVerifier: codeVerifier) {
-            present(SFSafariViewController(url: authorizationURL), animated: true)
-
-//            UIApplication.shared.open(authorizationURL)
-        }
-    }
-
-    
-    // The code verifier is a cryptographically random string using the characters A-Z, a-z, 0-9, and the punctuation characters -._~ (hyphen, period, underscore, and tilde), between 43 and 128 characters long
-    func generateCodeVerifier() -> String {
-        let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~"
-        let length = Int.random(in: 43...128)
-        var codeVerifier = ""
-        
-        for _ in 0..<length {
-            let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-            let index = characters.index(characters.startIndex, offsetBy: randomIndex)
-            codeVerifier.append(characters[index])
-        }
-        
-        return codeVerifier
-    }
-
-    // Once the client has generated the code verifier, it uses that to create the code challenge
-    func createCodeChallenge(from codeVerifier: String) -> String {
-        // Convert the code verifier to a SHA-256 hash.
-        // Convert the SHA-256 hash to a Base64-URL-encoded string.
-        return base64url(sha256(codeVerifier))
-    }
-
-    func sha256(_ input: String) -> Data {
-        let inputData = Data(input.utf8)
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        inputData.withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(inputData.count), &hash)
-        }
-        return Data(hash)
-    }
-
-    func base64url(_ data: Data) -> String {
-        var base64 = data.base64EncodedString()
-        base64 = base64.replacingOccurrences(of: "+", with: "-")
-        base64 = base64.replacingOccurrences(of: "/", with: "_")
-        base64 = base64.replacingOccurrences(of: "=", with: "")
-        return base64
-    }
-
-    
-    func buildAuthorizationURL(codeVerifier: String) -> URL? {
-        let authorizationBaseURLString = "https://isthereanydeal.com/oauth/authorize/"
-        
-        var components = URLComponents(string: authorizationBaseURLString)
-        components?.queryItems = [
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "client_id", value: "9247d05e4863edf2"),
-            URLQueryItem(name: "redirect_uri", value: "https://timmyTopaz://auth"),  // url to get back to app
-            URLQueryItem(name: "scope", value: "wait_read"),
-            URLQueryItem(name: "state", value: generateStateParameter()),
-            URLQueryItem(name: "code_challenge", value: codeVerifier),
-            URLQueryItem(name: "code_challenge_method", value: "S256")
-        ]
-        
-        let authorizationURL = components?.url
-        return authorizationURL
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
     
-    func generateStateParameter() -> String {
-        let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        let length = 16 // A typical length for the state parameter
-        var state = ""
-        
-        for _ in 0..<length {
-            let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-            let index = characters.index(characters.startIndex, offsetBy: randomIndex)
-            state.append(characters[index])
+    func didTapGoogleSignIn() -> UIAction {
+        return UIAction { _ in
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+            
+            // Create Google Sign In configuration object.
+            let config = GIDConfiguration(clientID: clientID)
+            GIDSignIn.sharedInstance.configuration = config
+            
+            // Start the google sign in flow!
+            GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+                guard error == nil,
+                      let user = result?.user,
+                      let idToken = user.idToken?.tokenString
+                else { return }
+    
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+                Auth.auth().signIn(with: credential) { result, error in
+                  // At this point, our user is signed in
+                    print("Signed in: \(result)")
+                }
+            }
         }
-        
-        return state
     }
 }
-
-// No, the redirect URI does not have to start with https://. When dealing with custom URL schemes like timmyTopaz://auth, you do not use https:// because you're not redirecting to a web URL but rather to a custom scheme registered by your iOS app.
